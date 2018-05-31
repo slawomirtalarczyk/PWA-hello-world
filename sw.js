e51a4b67d181db9b894e19843fbe6931c16d7102
Sławomir Talarczyk <slawomir.talarczyk@yahoo.com>
2018-05-31 18:46:01 +0200

    Basic SW

--- sw.js
+++ sw.js
@@ -1,6 +1,19 @@
-var cacheName = 'hello-world-page';
-var filesToCache = [
+var CACHE_NAME = 'hello-world-page';
+var urlsToCache  = [
   '/index.html',
   '/style.css',
   'lena.png'
 ];
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          console.log('Opened cache');
+          return cache.addAll(urlsToCache);
+        })
+    );
+  });
+  
+  
